3c5fa5e8138e0656b85008019373502efbb5dc27
mjofficials <manojmarimuthu1995@gmail.com>
2020-10-13 20:41:04 +0530

    added autoclose function

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-$('.filters ul li').click(function () {
-    $('.filters ul li').removeClass('active');
-    $(this).addClass('active');
 
-    var data = $(this).attr('data-filter');
-    $grid.isotope({
-        filter: data
-    })
-});
-var $grid = $(".gallery-grid").isotope({
-    itemSelector: ".all",
-    percentPosition: true,
-    masonry: {
-        columnWidth: ".all"
-    }
-})
-// Function used to show the image in the lightbox
-var zoomImg = function () {
-    // Create evil image clone
-    var clone = this.cloneNode();
-    clone.classList.remove("zoomD");
-    // Put evil clone into lightbox
-    var lb = document.getElementById("lb-img");
-    lb.innerHTML = "";
-    lb.appendChild(clone);
-    // Show lightbox
-    lb = document.getElementById("lb-back");
-    lb.classList.add("show");
-};
-window.addEventListener("load", function () {
-    // Attach on click events to all .zoomD images
-    var images = document.getElementsByClassName("zoomD");
-    if (images.length > 0) {
-        for (var img of images) {
-            img.addEventListener("click", zoomImg);
-        }
-    }
-    // Click event to hide the lightbox
-    document.getElementById("lb-back").addEventListener("click", function () {
-        this.classList.remove("show");
-    })
-});
 
 // 
 // Function used for navigation bar
@@ -91,3 +50,49 @@ window.addEventListener('scroll', function () {
     // console.log('Offset * 0.7' + offset)
     parallax.style.backgroundPositionY = offset * 0.4 + "px";
 })
+
+
+// GALLERY FILTER FUNCTIONS
+$('.filters ul li').click(function () {
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: data
+    })
+});
+var $grid = $(".gallery-grid").isotope({
+    itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+        columnWidth: ".all"
+    }
+})
+
+// Function used to show the image in the lightbox
+var zoomImg = function () {
+    // Create evil image clone
+    var clone = this.cloneNode();
+    clone.classList.remove("zoomD");
+    // Put evil clone into lightbox
+    var lb = document.getElementById("lb-img");
+    lb.innerHTML = "";
+    lb.appendChild(clone);
+    // Show lightbox
+    lb = document.getElementById("lb-back");
+    lb.classList.add("show");
+};
+window.addEventListener("load", function () {
+    // Attach on click events to all .zoomD images
+    var images = document.getElementsByClassName("zoomD");
+    if (images.length > 0) {
+        for (var img of images) {
+            img.addEventListener("click", zoomImg);
+        }
+    }
+    // Click event to hide the lightbox
+    document.getElementById("lb-back").addEventListener("click", function () {
+        this.classList.remove("show");
+    })
+});
